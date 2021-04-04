e2a635e7f1267ebeeecd1e19b02bff2a55d264c5
Nitesh Malhotra <niteshmalhotra00@gmail.com>
2021-04-04 11:23:49 +0530

    initial commit

---
+++ src/SelectionSort.java
@@ -0,0 +1,22 @@
+
+
+public class SelectionSort {
+    public static void main(String[] arg) {
+        int[] a ={3,44,10,2,5,42,11};
+        int n=a.length;
+        for(int i=1;i<n;i++)
+        {
+            int temp=a[i];
+            int j=i-1;
+            while(j>=0 && a[j]>temp)
+            {
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=temp;
+        }
+        for(int k=0;k<n;k++)
+        System.out.println(a[k]);
+
+    }
+}
