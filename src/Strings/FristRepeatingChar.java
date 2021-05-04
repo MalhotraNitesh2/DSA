fc51c3fed49747e454949aa9a4dfc017234102b9
Nitesh Malhotra <niteshmalhotra00@gmail.com>
2021-05-04 19:14:41 +0530

    intial commit

---
+++ src/Strings/FristRepeatingChar.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class FristRepeatingChar {
+    public static char  check(String s)
+    {
+        int res=Integer.MAX_VALUE;
+        int[] count=new int[256];
+        Arrays.fill(count,-1);
+        for(int i=0;i<s.length();i++)
+        {
+               if(count[s.charAt(i)]==-1)
+                   count[s.charAt(i)]=i;
+               else
+                   res=Math.min(res,count[s.charAt(i)]);
+        }
+        return s.charAt(res);
+    }
+    public static void main(String[] arg)
+    {
+            String s="abcbc";
+           System.out.println( check(s));
+
+    }
+}
