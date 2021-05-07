99868206cf1a8a3d1067b282030ee2b8ebe5a934
Nitesh Malhotra <niteshmalhotra00@gmail.com>
2021-05-07 21:43:25 +0530

    intial commit

---
+++ src/Strings/IPConfig.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class IPConfig {
+    public static void main(String[] arg)
+    {
+        String str="1.1.1.1";
+        String strnew=str.replace(".","[.]");
+         System.out.print(strnew);
+    }
+}
