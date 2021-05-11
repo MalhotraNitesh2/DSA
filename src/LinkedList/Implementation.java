d3d04071dbdfc772f6c04c737a7c6c6ab9471140
Nitesh Malhotra <niteshmalhotra00@gmail.com>
2021-05-11 14:00:50 +0530

    intial commit

--- src/LinkedList/Implementation.java
+++ src/LinkedList/Implementation.java
@@ -1,4 +1,34 @@
 package LinkedList;
 
 public class Implementation {
+    static class LinkList{
+        Node head;
+       static class Node{
+            int data;
+            Node next;
+            Node(int d){
+                data=d;
+                next=null;
+            }
+        }
+        static void display(Node node)
+        {
+             while(node !=null)
+             {
+                 System.out.println(node.data);
+                 node =node.next;
+             }
+        }
+    }
+    public static void main(String[]arg)
+    {
+        LinkList ll= new LinkList();
+        ll.head=new LinkList.Node(10);
+        LinkList.Node second= new LinkList.Node(20);
+        LinkList.Node third= new LinkList.Node(30);
+        ll.head.next=second;
+        second.next=third;
+        third.next=null;
+        LinkList.display(ll.head);
+    }
 }
