232f68950c9453357cbac05ebf4f1e659c814810
Nitesh Malhotra <niteshmalhotra00@gmail.com>
2021-05-20 16:26:30 +0530

    intial commit

---
+++ src/Stack/ReverseString.java
@@ -0,0 +1,28 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ReverseString {
+     static String reverString(String s)
+    {
+        char[] reversestring =new char[s.length()];
+        int len=  s.length();
+        Stack<Character> s1= new Stack<>();
+        for(int i=0;i<len;i++)
+        {
+              s1.push(s.charAt(i));
+        }
+        int i=0;
+        while(!s1.empty())
+        {
+             reversestring[i++]=s1.pop();
+        }
+        return new String(reversestring);
+    }
+    public static void main(String[] arg)
+    {
+        String str="Hey how you doing ";
+        System.out.println(reverString(str));
+
+    }
+}
